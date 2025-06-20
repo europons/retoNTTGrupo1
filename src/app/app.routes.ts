fcc2f136d04faeaa8d0aeff1ca20ed253d17cce1
RackGamesDev <yesterdiscord7@gmail.com>
2025-06-20 10:00:27 +0200

    Modulo home creado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '', //Al no poner nada se usa la raiz
+        //component: HomePageComponent, //El componente se puede cargar directamente aqui o en el modulo home
+        loadChildren: () => import('./modules/home/home-module').then(m => m.HomeModule) //En el caso de los modulos, se cargan lazy
+    }
+];
